2cdd1940b773b1d96127ef04c00b0ca73e3155fa
Vicky-peace <victorpeace47@gmail.com>
2024-10-04 09:12:39 +0300

    added highlighting characters feature

--- src/first-question.jsx
+++ src/first-question.jsx
@@ -31,12 +31,33 @@ export const FirstQuestion = () => {
   const [fruitsData, setFruitsData] = useState(fruits);
   const [searchTerm, setSearchTerm] = useState("");
 
+  // Event handler for input change
   const handleInputChange = (e) => {
     setSearchTerm(e.target.value);
   };
+
+  // Filter the fruits based on search term
   const fruitsDataFiltered = fruitsData.filter((fruit) =>
-    fruit.includes(searchTerm)
+    fruit.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
   );
+
+  //function to highlight matching characters
+  const highlightMatch = (fruit) => {
+    const regex = new RegExp(`(${searchTerm})`, "gi");
+    const parts = fruit.split(regex);
+    return(
+        <>
+        {parts.map((part, index) =>
+        part.toLowerCase() === searchTerm.toLowerCase() ? (
+            <strong key={index}>{part}</strong>
+        ): (
+            part
+        )
+        )}
+        </>
+    )
+
+  }
 
   return (
     <div>
@@ -45,10 +66,20 @@ export const FirstQuestion = () => {
         placeholder="Search here..."
         onChange={handleInputChange}
       />
-
-      {fruitsDataFiltered.map((fruit) => {
-        return <p>{fruit}</p>;
-      })}
+      <ul>
+      {fruitsDataFiltered.length > 0 ?(
+        fruitsDataFiltered.map((fruit, index)=> <li key={index}>{highlightMatch(fruit)}</li>)
+      ):(
+        <li>No matches found</li>
+      )}
+      </ul>
+     
     </div>
   );
 };
+
+
+/*Additional interviewer quizes
+1.To display something when nothing i showed e.g an error message
+2.Make each character or word you type in search bar be bold
+*/
